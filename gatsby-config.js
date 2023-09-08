edb0e09b2ee1a5501682bd95363c30026e42278c
Kelly Carino <mikaela.carino@gmail.com>
2023-09-08 15:42:07 -0700

    remove unused path

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/posts`,
-        name: `posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/projects`,
         name: `projects`,
       },
