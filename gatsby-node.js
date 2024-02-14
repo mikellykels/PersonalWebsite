3f28104dc55eded837d021f1e3ebcf89fb016301
Kelly Carino <mikaela.carino@gmail.com>
2024-02-13 18:23:08 -0800

    fix fonts again?

--- gatsby-node.js
+++ gatsby-node.js
@@ -31,7 +31,6 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       alias: {
         '@components': path.resolve(__dirname, 'src/components'),
         '@config': path.resolve(__dirname, 'src/config'),
-        '@fonts': path.resolve(__dirname, '/fonts'),
         '@images': path.resolve(__dirname, 'src/images'),
         '@pages': path.resolve(__dirname, 'src/pages'),
         '@styles': path.resolve(__dirname, 'src/styles'),
